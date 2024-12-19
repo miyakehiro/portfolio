c4857d9b78b17a49e178f01269b47f015445e7d8
miyakehiro <movix0824@gmail.com>
2024-12-19 12:13:58 +0900

    gulp調整

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ const connectSync = (done) => {
 
 // HTMLコンパイル（EJS）
 const compileHtml = () => {
-  const data = JSON.parse(fs.readFileSync('./_src/json/data.json'));
+  const data = JSON.parse(fs.readFileSync('./_src/json/data.json', 'utf8'));
   return gulp
     .src(src.ejs)
     .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
@@ -109,7 +109,6 @@ const watchFiles = (done) => {
   gulp.watch(src.sass, compileSass);
   gulp.watch(src.js, compileJs);
   gulp.watch(src.image, images);
-  gulp.watch('dist/**/*.html').on('change', browserSync.reload);
   done();
 };
 
